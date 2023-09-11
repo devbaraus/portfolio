132b311964474e6183b4d0feb4baed5180d6784e
Bruno de Araújo Alves <me@baraus.dev>
2023-09-11 00:03:25 -0300

    Fix site url

--- src/site.config.tsx
+++ src/site.config.tsx
@@ -32,7 +32,7 @@ export const siteName = 'BARAUS';
 export const siteDescription =
   'Looking for a freelance web developer to take your website to the next level? I am a freelance full stack web developer based in Brazil.';
 export const siteKeywords = 'portfolio, web developer, full stack, freelance';
-export const siteUrl = 'https://wwww.baraus.dev';
+export const siteUrl = 'https://www.baraus.dev';
 export const siteImage = `${siteUrl}/logo_og-image.png`;
 
 export const contact = [
