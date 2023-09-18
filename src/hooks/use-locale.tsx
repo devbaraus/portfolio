69f37480ff5f189bf3adb422647673e548bce15b
Bruno de Araújo Alves <me@baraus.dev>
2023-09-18 09:43:26 -0300

    Fix issue with locate

--- src/hooks/use-locale.tsx
+++ src/hooks/use-locale.tsx
@@ -2,8 +2,9 @@
 
 import { useParams } from 'next/navigation';
 import { Lang } from '@/types';
+import { defaultLocalePrefix } from '@/middleware';
 
 export function useLocale() {
   const params = useParams();
-  return params!.locale as Lang;
+  return (params!.locale || defaultLocalePrefix) as Lang;
 }
